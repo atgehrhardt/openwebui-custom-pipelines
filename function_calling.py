8a36ebc69d259fdc547de2d83872917a05dc8626
Andrew Tait Gehrhardt <atgehrhardt@gmail.com>
2024-06-08 02:54:01 -0400

    first commit

---
+++ function_calling.py
@@ -0,0 +1,60 @@
+import os
+import requests
+from typing import Literal, List, Optional
+from datetime import datetime
+
+from blueprints.function_calling_blueprint import Pipeline as FunctionCallingBlueprint
+
+class Pipeline(FunctionCallingBlueprint):
+    class Valves(FunctionCallingBlueprint.Valves):
+        # Add your custom parameters here
+        pass
+
+    class Tools:
+        def __init__(self, pipeline) -> None:
+            self.pipeline = pipeline
+
+        def get_current_time(
+            self,
+        ) -> str:
+            """
+            Get the current time.
+
+            :return: The current time.
+            """
+
+            now = datetime.now()
+            current_time = now.strftime("%H:%M:%S")
+            return f"Current Time = {current_time}"
+
+        def calculator(self, equation: str) -> str:
+            """
+            Calculate the result of an equation.
+
+            :param equation: The equation to calculate.
+            """
+
+            # Avoid using eval in production code
+            # https://nedbatchelder.com/blog/201206/eval_really_is_dangerous.html
+            try:
+                result = eval(equation)
+                return f"{equation} = {result}"
+            except Exception as e:
+                print(e)
+                return "Invalid equation"
+
+    def __init__(self):
+        super().__init__()
+        # Optionally, you can set the id and name of the pipeline.
+        # Best practice is to not specify the id so that it can be automatically inferred from the filename, so that users can install multiple versions of the same pipeline.
+        # The identifier must be unique across all pipelines.
+        # The identifier must be an alphanumeric string that can include underscores or hyphens. It cannot contain spaces, special characters, slashes, or backslashes.
+        # self.id = "my_tools_pipeline"
+        self.name = "My Tools Pipeline"
+        self.valves = self.Valves(
+            **{
+                **self.valves.model_dump(),
+                "pipelines": ["*"],  # Connect to all pipelines
+            },
+        )
+        self.tools = self.Tools(self)
